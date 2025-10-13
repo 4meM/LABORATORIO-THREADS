1df4fe37a44aee5e722d7aa1d69f866408f7fee7
4meM <eramosq@unsa.edu.pe>
2025-10-13 14:50:57 -0500

    intentando hacer una implementacion unica

--- linked-list.c
+++ linked-list.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 
 
+pthread_mutex_t lista = PTHREAD_MUTEX_INITIALIZER;
+
 struct Node {
     int data;
     struct Node* next;
@@ -10,14 +12,17 @@ struct Node {
 
 int insert_linked_list(struct Node** head, int value){
     struct Node* temp;
-    pthread_mutex_lock(&head);
-    temp = head;
-
-    while(temp != NULL || temp-> data < value){
-        
+    pthread_mutex_lock(&lista);
+    temp = *head;
+    while(temp != NULL && temp-> data < value){
+        temp = temp -> next;  
     }
 
-
+    if(temp != NULL && temp -> data == value){
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        new_node -> data = value;
+    }
+    pthread_mutex_unlock(&lista);
 
 }
 
